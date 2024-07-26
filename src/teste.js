ad4d203d9ba7b5237b01e9ec49a19576e6ff1df0
gmaeda <ossamumaeda@gmail.com>
2024-07-26 08:04:13 -0300

    Push teste

--- src/teste.js
+++ src/teste.js
@@ -1,10 +1,15 @@
 import {parse} from 'csv'
 import * as fs from 'node:fs'
 
-fs.createReadStream('Book1.csv')
+let delay = 0;
+
+createReadStream('Book')
   .pipe(parse({ delimiter: ';', columns: true }))
   .on('data', (row) => {
-    console.log(row);
+    delay += 500; // Delay of 500 milliseconds
+    setTimeout(() => {
+      console.log(row);
+    }, delay);
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
